a977c2a5468cf9639d7c7b77aaa318a3927841dd
Мария Сафи <safim@yandex.ru>
2023-07-26 19:57:16 +0500

    Add project files.

---
+++ DataBinding/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataBinding
+{
+    public partial class Form1 : Form
+    {
+        bool isBind = true;
+        public Form1()
+        {
+            InitializeComponent();
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+           
+        }
+
+     
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.DataBindings.Clear();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+                label1.DataBindings.Add(new Binding("Text", textBox1, "Text"));
+
+        }
+    }
+}
